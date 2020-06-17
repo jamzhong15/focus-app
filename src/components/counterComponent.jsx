ea0d91811d69ad7f0fbba86d3a4d3efa65df551b
James Zhong <jamzhong@gmail.com>
2020-06-17 17:59:01 +0800

    increment

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -3,15 +3,38 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
-        <span className="">{this.state.count}</span>
-        <button>Increment</button>
+        <span className={this.getCountColour()}>{this.state.count}</span>
+        <button onClick={this.handleIncrement} className="btn btn-info btn-sm">
+          Increment
+        </button>
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
+  }
+
+  getCountColour() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {}
